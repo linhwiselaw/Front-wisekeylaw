907323a76a6ef0259996de347a85cc5ee9ae945a
LongGoll <luuhoanglong21@gmail.com>
2022-04-27 15:34:24 +0700

    add

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000'
-		: 'https://backend-powerlaw.herokuapp.com'
+		: 'https://backend-wisekeylaw.herokuapp.com'
